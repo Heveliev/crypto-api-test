9f8732bdbfcde329a43bbd691c6df27fb00fd8de
Anatolii Heveliev <109628656+Heveliev@users.noreply.github.com>
2023-10-05 18:53:03 +0300

    fix some bugs

--- controllers/estimate/estimate.js
+++ controllers/estimate/estimate.js
@@ -4,53 +4,56 @@ const {modelExchange} = require("../../models");
 
 
 const estimate =  async (req, res) => {
-const  { inputAmount, inputCurrency, outputCurrency } = req.query;
+    const {inputAmount, inputCurrency, outputCurrency} = req.query;
 
-const coinFunc = (inp) => {
-    if(inp === "USDT") {
-        return outputCurrency;
-    }
-    else return inputCurrency
-}
 
-axios.all(
-    [
-        axios.get(`${modelExchange.binance.url}?symbol=${coinFunc(inputCurrency)}USDT`),
-        axios.get(`${modelExchange.kucoin.url}?symbol=${coinFunc(inputCurrency)}-USDT`)
-    ]
-)
-    .then(axios.spread((binance, kucoin) => {
-
-        const bitPrice = binance.data[0].price;
-        const kuPrice = kucoin.data.data.price;
-
-        const betterExchange = bitPrice >= kuPrice;
-
-        const exch = betterExchange ? "binance" : "kucoin";
-
-        const amount = (price) => {
-            if(inputCurrency === "USDT"){
-                return inputAmount/price;
-            }
-            else return price * inputAmount
+    const coinFunc = (inp,oup) => {
+        if(inp !== "USDT" && oup !== "USDT") {
+            return {fr:inp,sc:oup}
         }
+        if(inp === "USDT") {
+            return {fr:oup,sc:inp}
+        }
+        else return {fr:inp,sc:oup}
+    }
 
-        const amountPrice =    betterExchange ?  amount(bitPrice) : amount(kuPrice);
+    const {fr,sc} = coinFunc(inputCurrency,outputCurrency);
+    console.log(fr,sc)
 
-        const result = {
-            exchangeName: exch,
-            outputAmount: amountPrice.toFixed(2),
-        };
+    axios.all(
+        [
+            axios.get(`${modelExchange.binance.url}?symbol=${fr}${sc}`),
+            axios.get(`${modelExchange.kucoin.url}?symbol=${fr}-${sc}`)
+        ]
+    )
+        .then(axios.spread((binance, kucoin) => {
 
-        res.json(result);
-    }))
-    .catch(error => {
-        res.status(500).json({ error: 'Exchange server error' });
-    });
+            const bitPrice = binance.data[0].price;
+            const kuPrice = kucoin.data.data.price;
 
+            const betterExchange = bitPrice >= kuPrice;
 
-};
+            const exch = betterExchange ? "binance" : "kucoin";
 
+            const amount = (price) => {
+                if (inputCurrency === "USDT") {
+                    return inputAmount / price;
+                } else return price * inputAmount
+            }
 
+            const amountPrice = betterExchange ? amount(bitPrice) : amount(kuPrice);
+
+            const result = {
+                exchangeName: exch,
+                outputAmount: amountPrice.toFixed(2),
+            };
+
+            res.json(result);
+        }))
+        .catch(error => {
+            res.status(500).json({error: 'Exchange server error'});
+        });
+
+}
 
 module.exports = estimate;
